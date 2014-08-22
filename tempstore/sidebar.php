5b33ab218f114d0096b8f918884a5172b6a95bfd
katzkode <katzkode@gmail.com>
2014-08-22 10:06:13 -0400

    first commit

---
+++ tempstore/sidebar.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The Sidebar containing the main widget areas.
+ *
+ * @package Tonal
+ */
+?>
+	<nav id="site-navigation" class="main-navigation" role="navigation">
+		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'tonal' ); ?></a>
+		<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+	</nav><!-- #site-navigation .main-navigation -->
+
+	
